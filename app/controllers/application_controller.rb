88d05ff51da396d2c4ac16e78cb9053a12b5a7a2
DominiqueFlabbi <dominique.flabbi@gmail.com>
2020-05-02 12:16:46 -0400

    darkmode feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def moon
+    cookies[:moon] = {
+      value: 'dark mode on'
+    }
+    redirect_to welcome_index_path
+  end
+
+  def sun
+    cookies.delete(:moon)
+    redirect_to welcome_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  
+  get '/moon', to: 'application#moon', as: 'moon'
+  get '/sun', to: 'application#sun', as: 'sun'
   resources :articles do 
     resources :comments
   end
