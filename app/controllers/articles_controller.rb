56b0ea835edd2ad6d3907105687268af908acfeb
DominiqueFlabbi <dominique.flabbi@gmail.com>
2020-05-03 19:27:39 -0400

    modifying auth so folks can't read my posts; will need a password page in the future

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "dominiquefl", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "dominiquefl", password: "secret"
   
   def index
     @articles = Article.all
