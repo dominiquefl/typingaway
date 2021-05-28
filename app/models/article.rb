20369c1d4195434f865e03c792038e515d068210
Dominique Flabbi <dominique.flabbi@gmail.com>
2021-05-27 20:18:46 -0700

    update gemfile and dependent destroy on article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
-  has_many :comments, dependent: :destroy
-  has_many :taggings
+  has_many :comments, :dependent => :delete_all
+  has_many :taggings, :dependent => :delete_all
   has_many :tags, through: :taggings
   validates :title, presence: true, length: {minimum: 5}
 
 
-  def tag_list 
+  def tag_list
     tags.join(", ")
   end
 
@@ -15,4 +15,3 @@ class Article < ApplicationRecord
     self.tags = new_or_found_tags
   end
 end
-
