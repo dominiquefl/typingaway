0c9a98ea39aacfe88030f1bd28edfcea64102fea
Dominique Flabbi <dominique.flabbi@gmail.com>
2021-08-25 22:13:17 -0700

    added logging

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -4,8 +4,10 @@ class AuthorSessionsController < ApplicationController
 
   def create
     if login(params[:email], params[:password])
+      Rails.logger.info("[#{self.class}] User logged in successfully")
       redirect_back_or_to(articles_path, notice: 'Logged in successfully!')
     else
+      Rails.logger.info("[#{self.class}] User failed to login successfully")
       flash.now.alert = 'Login failed.'
       render action: :new
     end
